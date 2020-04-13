edaf7112d42015b4572ee0a2bef8f7941e187917
Owen Morgan <omorgan7@me.com>
2020-04-13 19:43:55 +0100

    Expand test cases.

--- tests/mmr_tests.js
+++ tests/mmr_tests.js
@@ -44,7 +44,7 @@ const assert = require('assert')
     for (let i = 0; i < 5; ++i)
     {
         updatedTeam.push({
-            mmr: 2087,
+            mmr: 2093,
             game_team: utilities.DIRE,
             matchCount: 9999,
             winStreak: false,
@@ -54,5 +54,57 @@ const assert = require('assert')
 
     mmr.updateMmrSystem(oldTeam, winner)
 
-    assert(oldTeam == updatedTeam)
+    assert(utilities.equals(oldTeam, updatedTeam))
+}
+
+{
+    const oldTeam = []
+    for (let i = 0; i < 5; ++i)
+    {
+        oldTeam.push({
+            mmr: 2000 + i * 10,
+            game_team: utilities.RADIANT,
+            matchCount: 9999,
+            winStreak: false,
+            lossStreak: false
+        })
+    }
+    for (let i = 0; i < 5; ++i)
+    {
+        oldTeam.push({
+            mmr: 2000 - i * 10,
+            game_team: utilities.DIRE,
+            matchCount: 9999,
+            winStreak: false,
+            lossStreak: false
+        })
+    }
+    
+    const winner = utilities.DIRE
+
+    const updatedTeam = []
+    for (let i = 0; i < 5; ++i)
+    {
+        updatedTeam.push({
+            mmr: 1975 + i * 10,
+            game_team: utilities.RADIANT,
+            matchCount: 9999,
+            winStreak: false,
+            lossStreak: false
+        })
+    }
+    for (let i = 0; i < 5; ++i)
+    {
+        updatedTeam.push({
+            mmr: 2025 - i * 10,
+            game_team: utilities.DIRE,
+            matchCount: 9999,
+            winStreak: false,
+            lossStreak: false
+        })
+    }
+
+    mmr.updateMmrSystem(oldTeam, winner)
+
+    assert(utilities.equals(oldTeam, updatedTeam))
 }
