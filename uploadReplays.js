fd1895bf991ed2afefda3b90c757f09103029013
Owen Morgan <omorgan7@me.com>
2020-05-11 19:51:02 +0100

    Rewrite the upload replays script to be serial.

--- uploadReplays.js
+++ uploadReplays.js
@@ -7,15 +7,27 @@ const fs = require('fs')
 const db = database.startDatabase()
 
 const path = "/Users/owen/Documents/replays/"
-const files = fs.readdirSync(path)
+let files = fs.readdirSync(path)
 files.sort()
+files = files.filter((file) => file.endsWith(".dem"))
 
-files.filter((file) => file.endsWith(".dem")).forEach((file) => {
+const length = files.length
+var i = 0
+
+const parse = async function() {
+    let file = files[i]
     parser.parseReplay(path + file, async (err, matchData) => {
         if (err) {
             console.log("Error: Replay parse error:", err)
             return
         }
+        
         await database.addMatch(db, matchData)
+        if (i + 1 < length) {
+            ++i
+            await parse()
+        }
     })
-})
+}
+
+parse().then(()=>{console.log("done")})
